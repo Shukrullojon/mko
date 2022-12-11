df873d479002a77e312a7cc158dfea86728b3662
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2022-12-11 11:51:00 +0500

    ok

--- database/seeders/MerchantTerminalSeeder.php
+++ database/seeders/MerchantTerminalSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pages\Merchant;
+use App\Models\Pages\MerchantTerminal;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class MerchantTerminalSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class MerchantTerminalSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $merchants = Merchant::get();
+        foreach($merchants as $merchant){
+            MerchantTerminal::create([
+                'merchant_id' => $merchant->id,
+                'merchant' => Str::random(8),
+                'terminal' => Str::random(8),
+                'balance' => 0,
+                'status' => 1,
+            ]);
+        }
     }
 }
