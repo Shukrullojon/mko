cfda08515a09e34fe9e01c9000eef425f359d7b4
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2022-12-12 17:38:23 +0500

    ok

---
+++ app/Services/AbsService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Gateway\AbsGateway;
+
+class AbsService
+{
+    public static function transaction($data){
+        return AbsGateway::fire('POST', 'api/v1/bank', [
+            "jsonrpc" => "2.0",
+            "id" => rand(10000,99999),
+            "method" => "iabs.transactions.create.transactions",
+            "params" => [
+                    "type" => $data['type'],
+                    "sender" => [
+                        "account" => $data['sender_account'],
+                        "codeFilial" => $data['sender_code_filial'],
+                        "tax" => $data['sender_tax'],
+                        "name" => $data['sender_name'],
+                    ],
+                    "recipient"=>[
+                        "account" => $data['recipient_account'],
+                        "codeFilial" => $data['recipient_code_filial'],
+                        "tax" => $data['recipient_tax'],
+                        "name" => $data['recipient_name'],
+                    ],
+                    "purpose" => $data['purpose'],
+                    "amount" => (float)$data['amount']
+                ]
+        ]);
+    }
+
+}
