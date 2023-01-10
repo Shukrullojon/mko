a875771f2ce61a49f0d58bf69c2d458f5e6f521b
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2023-01-10 12:21:23 +0500

    ok

--- resources/views/pages/home/show.blade.php
+++ resources/views/pages/home/show.blade.php
@@ -2,19 +2,6 @@
 
 @section('content')
 
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}">@lang('global.home')</a></li>
-                        <li class="breadcrumb-item active">@lang('global.show')</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </section>
     <section class="content">
         <div class="row">
             <div class="col-md-12">
