516ee780a46e6868824cef393f48f788450af6bb
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2023-02-10 16:14:40 +0500

    fsdnf

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -5,63 +5,108 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Pages\Account;
 use App\Models\Pages\Transaction;
+use App\Models\Pages\TransactionAccount;
 use App\Services\AbsService;
+use App\Services\CardService;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    public function send($params){
-        $transaction = Transaction::where('id',$params['params']['transaction_id'])->where('type',0)->where('is_sent',0)->where('status',0)->first();
-        if(empty($transaction)){
+    public function send($params)
+    {
+        try {
+            $transaction = Transaction::where('id', $params['params']['transaction_id'])->where('type', 0)->where('is_sent', 0)->where('status', 1)->first();
+            $account = Account::where('type', 2)->first();
+            $bAccount = Account::where('type', 5)->first();
+
+            $tr1 = TransactionAccount::create([
+                'transaction_id' => $transaction->id,
+                'sender_id' => $account->id,
+                'receiver_id' => $transaction->account->id,
+                'amount' => ($transaction->amount * (100 - $bAccount->percentage)) / 100,
+                'transactionId' => "",
+                'status' => 0,
+            ]);
+
+            $tr2 = TransactionAccount::create([
+                'transaction_id' => $transaction->id,
+                'sender_id' => $account->id,
+                'receiver_id' => $bAccount->id,
+                'amount' => ($transaction->amount * $bAccount->percentage) / 100,
+                'transactionId' => "",
+                'status' => 0,
+            ]);
+
+            $debit = CardService::debit([
+                'amount' => $transaction->amount,
+                'token' => $transaction->receiver_card,
+            ]);
+            if ($debit) {
+                $transaction->update([
+                    'is_sent' => 1,
+                ]);
+
+                $trAccount = TransactionAccount::where('status',0)->get();
+                foreach ($trAccount as $tr){
+                    var_dump([
+                        'type' => "101",
+                        'sender_account' => $tr->sender->number,
+                        'sender_code_filial' => $tr->sender->filial,
+                        'sender_tax' => $tr->sender->inn,
+                        'sender_name' => $tr->sender->name,
+                        'recipient_account' => $tr->receiver->number,
+                        'recipient_code_filial' => $tr->receiver->filial,
+                        'recipient_tax' => $tr->receiver->inn,
+                        'recipient_name' => $tr->receiver->name,
+                        'purpose' => [
+                            "code" => "00668",
+                            "name" => "перевод (дата: " . date("Y-m-d H:i:s") . ") " . "} ID{V" . str_pad($tr->transaction_id, 12, '0', STR_PAD_LEFT) . "V}",
+                        ],
+                        'amount' => $tr->amount,
+                    ]);
+                    /*$abs = AbsService::transaction([
+                        'type' => "101",
+                        'sender_account' => $tr->sender->number,
+                        'sender_code_filial' => $tr->sender->filial,
+                        'sender_tax' => $tr->sender->inn,
+                        'sender_name' => $tr->sender->name,
+                        'recipient_account' => $tr->receiver->number,
+                        'recipient_code_filial' => $tr->receiver->filial,
+                        'recipient_tax' => $tr->receiver->inn,
+                        'recipient_name' => $tr->receiver->name,
+                        'purpose' => [
+                            "code" => "00668",
+                            "name" => "перевод (дата: " . date("Y-m-d H:i:s") . ") " . "} ID{V" . str_pad($tr->transaction_id, 12, '0', STR_PAD_LEFT) . "V}",
+                        ],
+                        'amount' => $tr->amount,
+                    ]);
+                    if(isset($abs['status']) and $abs['status']){
+                        $tr->update([
+                            'status' => 1,
+                            'transactionId' => $abs['data']['responseBody']['createdDocuments'][0]['transactionId'],
+                        ]);
+                    }*/
+                }
+            }else{
+                $tr1->update([
+                    'status' => 11,
+                ]);
+                $tr2->update([
+                    'status' => 11,
+                ]);
+            }
+
+        } catch (\Exception $exception) {
             return [
                 "error" => [
-                    "code" => 500,
+                    "code" => 501,
                     "message" => [
-
+                        'uz' => $exception->getMessage(),
+                        'ru' => $exception->getMessage(),
+                        'en' => $exception->getMessage(),
                     ],
                 ],
             ];
         }
-        $account = Account::where('type', 2)->first();
-        $bAccount = Account::where('type',5)->first();
-        /*$abs = AbsService::transaction([
-            'type' => "101",
-            'sender_account' => $account->number,
-            'sender_code_filial' => $account->filial,
-            'sender_tax' => $account->inn,
-            'sender_name' => $account->name,
-            'recipient_account' => $transaction->account->number,
-            'recipient_code_filial' => $transaction->account->filial,
-            'recipient_tax' => $transaction->account->inn,
-            'recipient_name' => $transaction->account->name,
-            'purpose' => [
-                "code" => "00668",
-                "name" => "перевод (дата: " . date("Y-m-d H:i:s") . ") "."} ID{V".str_pad($transaction->id,12,'0',STR_PAD_LEFT)."V}",
-            ],
-            'amount' => ($transaction->amount * (100 -  $bAccount->percentage))/100,
-        ]);
-
-        $absBank = AbsService::transaction([
-            'type' => "101",
-            'sender_account' => $account->number,
-            'sender_code_filial' => $account->filial,
-            'sender_tax' => $account->inn,
-            'sender_name' => $account->name,
-            'recipient_account' => $bAccount->number,
-            'recipient_code_filial' => $bAccount->filial,
-            'recipient_tax' => $bAccount->inn,
-            'recipient_name' => $bAccount->name,
-            'purpose' => [
-                "code" => "00668",
-                "name" => "перевод (дата: " . date("Y-m-d H:i:s") . ") "."} ID{V".str_pad($transaction->id,12,'0',STR_PAD_LEFT)."V}",
-            ],
-            'amount' => ($transaction->amount*$bAccount->percentage) / 100,
-        ]);*/
-
-        $transaction->update([
-            'is_sent' => 5,
-        ]);
-        return [];
-
     }
 }
